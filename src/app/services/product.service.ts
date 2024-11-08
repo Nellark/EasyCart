95056df705c6f1c33616f7e9a95f1ef652389864
Nellark <nelakulati@gmail.com>
2024-11-08 16:19:20 +0200

    added home page

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment.development';  // Ensure this path is correct
+import { environment } from '../../environments/environment.development';
 import { ProductsResponseInterface, ProductInterface } from '../types/Product.interface';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ProductService {
   constructor(private http: HttpClient) {}
 
   getAllProducts() {
-
     return this.http.get<ProductsResponseInterface>(environment.SERVER);
   }
 
   getProduct(product_id: string) {
-
     return this.http.get<ProductInterface>(`${environment.SERVER}/${product_id}`);
   }
 }
